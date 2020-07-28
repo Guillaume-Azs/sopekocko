d6f1e2384b4c7fadfc907b08a50cef4677f7bbc7
Guillaume Azas <guillaume.azs@MacBook-Air-de-Guillaume.local>
2020-07-28 15:55:54 +0200

    gestion erreur eslint2

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,16 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "udl*VFMnxp5Crly-({");
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'udl*VFMnxp5Crly-({');
     const userId = decodedToken.userId;
     if (!userId) {
-      throw "Merci de vous connecter";
+      throw 'Merci de vous connecter';
     } else {
       next();
     }
   } catch (error) {
-    res.status(401).json({ error: error | "Requête non authentifiée ! " });
+    res.status(401).json({ error: error | 'Requête non authentifiée ! ' });
   }
 };
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,20 @@
-const multer = require("multer");
+const multer = require('multer');
 
 const MIME_TYPES = {
-  "images/jpg": "jpg",
-  "images/jpeg": "jpg",
-  "images/png": "png",
+  'images/jpg': 'jpg',
+  'images/jpeg': 'jpg',
+  'images/png': 'png',
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "images");
+    callback(null, 'images');
   },
   filname: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
+    const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
+    callback(null, name + Date.now() + '.' + extension);
   },
 });
 
-module.exports = multer({ storage: storage }).single("image");
+module.exports = multer({ storage }).single('image');
